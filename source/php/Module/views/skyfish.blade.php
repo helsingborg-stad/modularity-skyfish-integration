d17bed0483086dfad5de3c0a8f40e23ee0fb747a
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-10-08 12:52:13 +0200

    Modularity module title

--- source/php/Module/views/skyfish.blade.php
+++ source/php/Module/views/skyfish.blade.php
@@ -1,1 +1,11 @@
-<div id="skyfish-module"></div>
+<div class="grid">
+    @if (!$hideTitle && !empty($post_title))
+    <div class="grid-xs-12 u-mb-4">
+        <h4 class="box-title">{!! apply_filters('the_title', $post_title) !!}</h4>
+    </div>
+    @endif
+    <div class="grid-xs-12">
+        <div id="skyfish-module"></div>
+    </div>
+</div>
+
